a509f7e46156573b20c2f01b99e86352d7eb77a6
ImperialDuck-dev <73429982+ImperialDuck-dev@users.noreply.github.com>
2022-06-12 23:34:42 +0800

    Add files via upload

--- E-Commerce/ECom/header.php
+++ E-Commerce/ECom/header.php
@@ -15,6 +15,12 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!--custom css file-->
     <link rel="stylesheet" href="style.css">
+
+    <?
+        //require functions.php
+        require('functions.php');
+    ?>
+
 </head>
 <body>
 <header id="header">
