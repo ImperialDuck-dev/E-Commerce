836d8f6eee81eb25e4308614f9a0cea50392596f
ImperialDuck-dev <mjmuyco13@gmail.com>
2022-06-15 02:29:19 +0800

    Update header.php

--- E-Commerce/ECom/header.php
+++ E-Commerce/ECom/header.php
@@ -24,17 +24,26 @@
 </head>
 <body>
 <header id="header">
-    <div class=" text-center   px-4 py-1 bg-light ">
-        <p class="font-rale font-size-12 text-black-50 m-1">5 Star Customer Ratings&nbsp&nbsp&nbsp&nbsp&nbsp | &nbsp&nbsp&nbsp&nbsp&nbspOverseas Delivery&nbsp&nbsp&nbsp&nbsp&nbsp | &nbsp&nbsp&nbsp&nbsp&nbspBest Quality Vehicles</p>
+    <div class="strip d-flex justify-content-between px-4 py-1 bg-light">
+        <p class="font-rale font-size-12 text-black-50 m-0">E Commerce Website.</p>
+        <div class="font-rale font-size-14">
+            <a href="#" class="px-3 border-right- border-left text-dark">LOGIN</a>
+        </div>
     </div>
-    <nav class="navbar  navbar-expand-lg navbar-dark color-gray-bg font-size-30">
+    <nav class="navbar navbar-expand-lg navbar-dark color-gray-bg">
         <div class="container-fluid">
+<<<<<<< HEAD
             <img src="Logos%20Revised.png" alt="" width="100" height="60" class="d-inline-block mx-2" ></a>
+=======
+            <a class="navbar-brand" href="#">
+                <img src="Logos.png" alt="" width="30" height="30" class="d-inline-block align-text-mid ">   Montemayor Car Dealership</a>
+>>>>>>> parent of cb19ad2 (Revert "Revert "Experimental Template"")
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav m-auto font-rubik">
+<<<<<<< HEAD
 
                 <ul class="navbar-nav m-auto p-4 font-rubik color-white">
 
@@ -51,17 +60,37 @@
                     </li>
                     <li class=" nav-item mx-5">
                         <a class="nav-link active" href="#">CONTACT</>
+=======
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">VEHICLE LIST</a>
                     </li>
 
-
-
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">PRICE LIST</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">NEWS AND EVENTS<i class="fas fa-chevron-down"></i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">CONTACT US</a>
+>>>>>>> parent of cb19ad2 (Revert "Revert "Experimental Template"")
+                    </li>
                 </ul>
+<<<<<<< HEAD
                 <div class="login_cart font-rale  font-size-30 align-left">
                     <i class="fas  fa-magnifying-glass color-white mx-3" href="#"></i>
                     <i class="fas  fa-truck-moving color-white mx-3" href="#"></i>
                     <i class="fas  fa-user color-white mx-3" href="#"></i>
                 </div>
 
+=======
+                <form action="#" class="font-size-14 font-rale">
+                    <a href="#" class="py-2 rounded-pill color-black-bg">
+                        <span class="font-size-16 px-2 text-white"><i class="fas fa-car-side"></i></span>
+                        <span class="px-3 py-2 rounded-pill text-dark bg-light">0</span>
+                    </a>
+                </form>
+>>>>>>> parent of cb19ad2 (Revert "Revert "Experimental Template"")
             </div>
         </div>
     </nav>
