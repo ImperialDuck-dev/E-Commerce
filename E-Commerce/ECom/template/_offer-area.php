90d21de3b672f56d127e950727879f31d1ba0a6e
ImperialDuck-dev <mjmuyco13@gmail.com>
2022-06-15 17:08:38 +0800

    Revert "Revert "Updated Experimental""
    This reverts commit 1743b80af561a124014cc4f6e5aeb6db46a31dab.

--- E-Commerce/ECom/template/_offer-area.php
+++ E-Commerce/ECom/template/_offer-area.php
@@ -1,17 +1,23 @@
 <section id="offer">
     <div class="container">
+<<<<<<< Updated upstream
         <h4 class="font-rubik font-size-20">SPECIAL OFFER!</h4>
         <div id="filters"class="button-group text-right">
+=======
+        <h4 class="font-rubik text-center font-size-40">SPECIAL OFFER</h4>
+        <div id="filters"class="button-group text-right font-rale">
+>>>>>>> Stashed changes
             <button class="btn all" data-filter="*">ALL CARS</button>
-            <button class="btn" data-filter=".Honda">Honda</button>
-            <button class="btn" data-filter=".Toyota">Toyota</button>
-            <button class="btn" data-filter=".Hyundai">Hyundai</button>
+            <button class="btn" data-filter=".MINI">MINI SUV</button>
+            <button class="btn" data-filter=".OFF-ROAD">OFF-ROAD SUV</button>
+            <button class="btn" data-filter=".BIG">BIG SUV</button>
         </div>
 
         <div class="grid">
-            <div class="grid-cars Honda border">
+            <div class="grid-cars OFF-ROAD ">
                 <div class="cars py-2" style="width:200px;">
                     <div class="item font-rale">
+<<<<<<< Updated upstream
                         <a><img src="../ECom/assets/cars/HONDA%20ACCORD.jpg" alt="cars1" class="img-fluid"</a>
                         <div class="text-center ">
                             <h6>HONDA ACCORD</h6>
@@ -22,6 +28,12 @@
                                 <span><i class="fas fa-star"></i></span>
                                 <span><i class="far fa-star"></i></span>
                             </div>
+=======
+                        <a><img src="./assets/cars/FJ%20Cruiser.jpg" alt="cars1" class="img-fluid"</a>
+                        <div class="text-center ">
+                            <h6>TOYOTA CRUISER</h6>
+
+>>>>>>> Stashed changes
                             <div class="price py-2">
                                 <span>$3000</span>
                             </div>
@@ -30,6 +42,7 @@
                     </div>
                 </div>
             </div>
+<<<<<<< Updated upstream
             <div class="grid-cars Toyota border">
                 <div class="cars py-2" style="width:200px;">
                     <div class="item font-rale">
@@ -51,6 +64,9 @@
                     </div>
                 </div>
             </div>
+=======
+
+>>>>>>> Stashed changes
         </div>
     </div>
 </section>
