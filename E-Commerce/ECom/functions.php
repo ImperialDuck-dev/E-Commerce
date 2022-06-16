10d99dda309bf7eba44db49d6cc22dfbddbc5502
ImperialDuck-dev <mjmuyco13@gmail.com>
2022-06-16 18:13:19 +0800

    Updated Website

--- E-Commerce/ECom/functions.php
+++ E-Commerce/ECom/functions.php
@@ -1,15 +1,15 @@
 <?php
 //require MYSQL connection
-require ('database/DBController.php');
+include ('database/DBController.php');
 
 //require car class
-require ('database/cars.php');
-
+include ('database/cars.php');
 
 //DBController object
 $db = new DBController();
 
 //Car object
-$product = new cars($db);
 
+$product = new cars($db);
 print_r($product->getCar_Data());
+?>
